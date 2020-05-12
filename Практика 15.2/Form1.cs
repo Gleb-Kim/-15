d2737d42a8b931888739c989a7dabb68d38d9f0d
Gleb <kimgleb2004@gmail.com>
2020-05-12 17:11:30 +0300

    Summary
    Description

---
+++ Практика 15.2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_15._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double res;
+            label2.Text = "Указанное число миль равно: ";
+            if(Double.TryParse(textBox1.Text, out res))
+            {
+                label2.Text = label2.Text + Convert.ToString(res * 1.61) + " км";
+            }  
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Практика 15.3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_15._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "НАЖМИ МЕНЯ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text != "Вы нажа-а-а-а-ли меня!!!!")
+                button1.Text = "Вы нажа-а-а-а-ли меня!!!!";
+            else
+                Close();
+        }
+    }
+}
